df5adde185fca67efbd1dc2e2e76a90cdd78e69f
Roger Adams <adamsrog04@gmail.com>
2015-03-01 15:47:37 -0800

    add image format support

--- app/components/placehold-it.js
+++ app/components/placehold-it.js
@@ -3,17 +3,23 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	height: 100,
 	width: 100,
-	text: undefined,
 
 	backgroundColor: 'aaa',
 	textColor: '666',
 
-	src: Ember.computed('height', 'width', 'backgroundColor', 'textColor', function() {
+	format: undefined, // gif, jpg, jpeg, png
+	text: undefined,
+
+	src: Ember.computed('height', 'width', 'backgroundColor', 'textColor', 'format', function() {
 		
 		// build url for placeholder image
 		var base = 'http://placehold.it/';
 		var src = base + this.get('width') + 'x' + this.get('height') + '/';
 		src += this.get('backgroundColor') + '/' + this.get('textColor');
+
+		if (this.get('format')) {
+			src += '.' + this.get('format');
+		}
 		
 		// check for custom placeholder text
 		if (this.get('text')) {
